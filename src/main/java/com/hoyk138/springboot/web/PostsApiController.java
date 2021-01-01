e4e192c21f16fd4e25df73e26ac560e6c57e0ea2
Kim <hoyk138@gmail.com>
2021-01-02 00:47:13 +0900

    4.3 게시글 등록 화면 만들기까지 완료

---
+++ src/main/java/com/hoyk138/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.hoyk138.springboot.web;
+
+import com.hoyk138.springboot.service.post.PostsService;
+import com.hoyk138.springboot.web.dto.PostsResponseDto;
+import com.hoyk138.springboot.web.dto.PostsSaveRequestDto;
+import com.hoyk138.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
