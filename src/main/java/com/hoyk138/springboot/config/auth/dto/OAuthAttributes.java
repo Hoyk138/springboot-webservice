aa6b81aef20739a4683dbb96ba4525afab6cbe13
Kim <hoyk138@gmail.com>
2021-01-04 22:08:30 +0900

    신규 회원의 권한을 GUEST에서 USER로 수정

--- src/main/java/com/hoyk138/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/hoyk138/springboot/config/auth/dto/OAuthAttributes.java
@@ -63,7 +63,8 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                //.role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
